8fd5a2d68778b86c4dc5433eda56db6789f2f9e1
Felicitas <fmarolda@udesa.edu.ar>
2024-06-03 16:54:42 -0300

    graf

--- Ejercicio2_1.py
+++ Ejercicio2_1.py
@@ -10,16 +10,13 @@ v1 = Vt[0]
 v1 = v1.reshape(28,28)
 axs[0].imshow(v1, cmap='gray')
 axs[0].axis('off')
-axs[0].set_title('Primer vector singular')
 v2 = Vt[1]
 v2 = v2.reshape(28,28)
 axs[1].imshow(v2, cmap='gray')
 axs[1].axis('off')
-axs[1].set_title('Segundo vector singular')
 v3 = Vt[2]
 v3 = v3.reshape(28,28)
 axs[2].imshow(v3, cmap='gray')
 axs[2].axis('off')
-axs[2].set_title('Tercer vector singular')
 plt.show()
 
--- Ejercicio2_2.py
+++ Ejercicio2_2.py
@@ -28,7 +28,7 @@ for i in range(18):
     plt.subplot(3, 6, i+1)
     plt.imshow(X[i].reshape(p, p), cmap='gray')
     plt.axis('off')
-plt.suptitle('Imágenes originales')
+# plt.suptitle('Imágenes originales')
 plt.show()
 
 #matriz de similaridad de x
@@ -46,45 +46,84 @@ for i in range(18):
     plt.subplot(3, 6, i+1)
     plt.imshow(X_k[i].reshape(p, p), cmap='gray')
     plt.axis('off')
-plt.suptitle('Imágenes reconstruidas')
+# plt.suptitle('Imágenes reconstruidas')
 plt.show()
 
 #matriz de similaridad de X_k
 mat_simil_X_k = fa.matriz_de_similitud(X_k,1000)
 
+X_1 = fa.compresion(X,1)
+X_3 = fa.compresion(X,3)
+X_5 = fa.compresion(X,5)
+X_8 = fa.compresion(X,8)
+X_10 = fa.compresion(X,10)
+X_15 = fa.compresion(X,15)
+
+#los grafiplt.figure()
+plt.figure(figsize=(18, 6))  # Ajusta el tamaño de la figura para que pueda acomodar todas las imágenes
+for n in range(1, 7):
+    for i in range(18):
+        plt.subplot(6, 18, (n-1)*18+i+1)  # Crea un subplot para cada imagen
+        if n == 1:
+            plt.imshow(X[i].reshape(p, p), cmap='gray')
+            if i == 0:
+                plt.title("Dimension: p")
+        elif n == 6:
+            plt.imshow(X_1[i].reshape(p, p), cmap='gray')
+            if i == 0:
+                plt.title("Dimension: p_1")
+        elif n == 5:
+            plt.imshow(X_3[i].reshape(p, p), cmap='gray')
+            if i == 0:
+                plt.title("Dimension: p_3")
+        elif n == 4:
+            plt.imshow(X_5[i].reshape(p, p), cmap='gray')
+            if i == 0:
+                plt.title("Dimension: p_5")
+        elif n == 3:
+            plt.imshow(X_10[i].reshape(p, p), cmap='gray')
+            if i == 0:
+                plt.title("Dimension: p_10")
+        elif n == 2:
+            plt.imshow(X_15[i].reshape(p, p), cmap='gray')
+            if i == 0:
+                plt.title("Dimension: p_15")
+        plt.axis('off')
+plt.show()
+
 #Graficar
 
-plt.figure()
-plt.imshow(mat_simil_X_k, cmap='viridis')
-plt.colorbar()
-plt.title('Matriz de Similitud')
-plt.xlabel('Índice')
-plt.ylabel('Índice')
-plt.show()
+# plt.figure()
+# plt.imshow(mat_simil_X_k, cmap='viridis')
+# plt.colorbar()
+# plt.title('Matriz de Similitud')
+# plt.xlabel('Índice')
+# plt.ylabel('Índice')
+# plt.show()
 
-plt.figure()
-plt.imshow(mat_simil_X_k, cmap='viridis')
-plt.colorbar()
-plt.title('Matriz de Similitud')
-plt.xlabel('Índice')
-plt.ylabel('Índice')
-plt.show()
+# plt.figure()
+# plt.imshow(mat_simil_X_k, cmap='viridis')
+# plt.colorbar()
+# plt.title('Matriz de Similitud')
+# plt.xlabel('Índice')
+# plt.ylabel('Índice')
+# plt.show()
 
-#graficar esta diferencia para distintos valores de k
-k_values = np.arange(1, 30)
-diferencias = np.zeros(k_values.shape)
-for i, k in enumerate(k_values):
-    X_k = fa.compresion(X, k)
-    diferencias[i] = np.linalg.norm(X-X_k)
-plt.figure()
-plt.plot(k_values, diferencias, color='hotpink')
-plt.xlabel('k')
-plt.ylabel('Diferencia')
-plt.title('Diferencia entre las imágenes originales y las reconstruidas')
-plt.grid()
-plt.show()
+# #graficar esta diferencia para distintos valores de k
+# k_values = np.arange(1, 30)
+# diferencias = np.zeros(k_values.shape)
+# for i, k in enumerate(k_values):
+#     X_k = fa.compresion(X, k)
+#     diferencias[i] = np.linalg.norm(X-X_k)
+# plt.figure()
+# plt.plot(k_values, diferencias, color='hotpink')
+# plt.xlabel('k')
+# plt.ylabel('Diferencia')
+# plt.title('Diferencia entre las imágenes originales y las reconstruidas')
+# plt.grid()
+# plt.show()
 
 
-#calcular el rango de x
-rango = np.linalg.matrix_rank(X)
-# print(f'El rango de la matriz X es: {rango}')
+# #calcular el rango de x
+# rango = np.linalg.matrix_rank(X)
+# # print(f'El rango de la matriz X es: {rango}')
