afafdb987bdb878683187e8ad645a6aeb38099b9
mariagonzalezdelsolar <gonzalezdelsolarm@udesa.edu.ar>
2024-06-18 16:35:37 -0300

    funciona el grafico de cambio de delta

--- TP4/Ejercicio.py
+++ TP4/Ejercicio.py
@@ -1,14 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import numpy as np
 
 n = 5
 d = 100
 # Definimos A y b aleatorios
 # Generamos A de n filas y d columnas 
-A = np.random.rand(n,d)
+A = np.random.randn(n,d)
 # Generamos b de n filas y 1 columna
-b = np.random.rand(n,1)
+b = np.random.randn(n,1)
 # Sigma son los valores singulares de A
 sigma = np.linalg.svd(A)[1] #OJO
 # definimos delta
@@ -22,27 +21,40 @@ lamda_ = np.linalg.eig(H)[0]
 s = 1 / max(lamda_) if max(lamda_) > 0 else 1
 
 # Definimos la función de costo F2 (se le agrega un término de regularización)
-def F(x, A, b, delta=0):
-    return (A @ x - b).T @ (A @ x - b) + delta * np.linalg.norm(x, 2)**2
+def F1(x, A, b):
+    return (A @ x - b).T @ (A @ x - b)
 
-# Definimos el gradiente de F1
-def gradiente_de_F(x, A, b, delta):
+def F2(x, A, b, delta):
+    return (A @ x - b).T @ (A @ x - b) + delta * x.T @ x
+
+def gradiente_F1(x, A, b):
+    return 2 * A.T @ A @ x - 2 * A.T @ b
+
+def gradiente_F2(x, A, b, delta):
     return 2 * A.T @ A @ x - 2 * A.T @ b + 2 * delta * x
 
-x_values = []
-
 # Definimos el metodo de descenso de gradiente iterativo para f1
-def descenso_gradiente(x1, s, cant_iteraciones, A, b, F, delta=0) -> tuple:
-    costos = [] 
+def descenso_gradiente_F1(x1, s, cant_iteraciones, A, b):
+    """CALCULA EL DESCENSO GRADIENTE DE UNA FUNCION F1"""
     x = x1
-    x_values.append(x.copy()) 
-    for i in range(cant_iteraciones):
-        x.real = x - s * gradiente_de_F(x, A, b, delta)
-        costo = F(x, A, b, delta)
-        costos.append(float(costo))
-        x_values.append(x.copy())
-    return x, costos
+    x_values = [x]
+    costos = []
+    for _ in range(cant_iteraciones):
+        x = x - s * gradiente_F1(x, A, b)
+        x_values.append(x)
+        costos.append(F1(x, A, b))
+    return x, costos, x_values
 
+def descenso_gradiente_F2(x1, s, cant_iteraciones, A, b, delta):
+    """CALCULA EL DESCENSO GRADIENTE DE UNA FUNCION F2"""
+    x = x1
+    x_values = [x]
+    costos = []
+    for _ in range(cant_iteraciones):
+        x = x - s * gradiente_F2(x, A, b, delta)
+        x_values.append(x)
+        costos.append(F2(x, A, b, delta))
+    return x, costos, x_values
 
 # Definimos la condición inicial aleatoriamente
 x1 = np.random.rand(d,1)
@@ -51,20 +63,25 @@ x1 = np.random.rand(d,1)
 cant_iteraciones = 1000
 
 # Calculamos el x que minimiza F1
-x, costos = descenso_gradiente(x1, s, cant_iteraciones, A, b, F)
+x, costos, x_values1 = descenso_gradiente_F1(x1, s, cant_iteraciones, A, b)
 # print(x)
 
+costos = np.array(costos).flatten()
+
 # Evaluamos F1 en x
-print(F(x, A, b))
+print(F1(x, A, b))
 # Evaluamos F1 en un x aleatorio
-print(F(np.random.rand(d,1), A, b))
+print(F1(np.random.rand(d,1), A, b))
 #Calculamos el error de A*x-b
 error = np.linalg.norm(A @ x - b)
 print("Error de A*x-b: ", error)
 
 
 # Hallamos el x que minimiza F2
-x_2, costos_f2 = descenso_gradiente(x1, s, cant_iteraciones, A, b, F, delta)
+x_2, costos_f2, x_values2 = descenso_gradiente_F2(x1, s, cant_iteraciones, A, b, delta)
+
+costos_f2 = np.array(costos_f2).flatten()
+
 # Calculamos el error de A*x-b
 error_2 = np.linalg.norm(A @ x_2 - b)
 print("Error de A*x-b con regularización: ", error_2)
@@ -90,58 +107,23 @@ x_svd = Vt.T @ S_inv @ U.T @ b
 #Calculamos el error de A*x-b
 error_svd = np.linalg.norm(A @ x_svd - b)
 print("Error de A*x-b con SVD: ", error_svd)
+deltas = [10**-5,10**-4, 10**-3, 10**-2, 10**-1]
 
-# Grafico de barras comparando los errores de los tres métodos en la iteración final
-fig, axs = plt.subplots(2)  # Crear una figura con dos subgráficos
+for delta in deltas:
+    x1_copy = np.copy(x1)
+    x_2, costos_f2, x = descenso_gradiente_F2(x1, s, cant_iteraciones, A, b, delta)
+    costos_f2 = np.array(costos_f2).flatten()
+    plt.plot(range(cant_iteraciones), costos_f2, label=f'delta = {delta}')
 
-# Primer subgráfico para el descenso de gradiente y SVD
-axs[0].bar(["Descenso de gradiente", "SVD"], [error, error_svd], color=['mediumorchid', 'royalblue'])
-axs[0].set_ylabel('Error')
-
-# Segundo subgráfico para el error de regularización
-axs[1].bar(["Descenso de gradiente con regularización"], [error_2], color=['hotpink'])
-axs[1].set_ylabel('Error')
-
-plt.tight_layout()  # Ajustar el layout para que no se superpongan los gráficos
+plt.xlabel('Iteraciones')
+plt.ylabel('Costo')
+plt.legend()
+plt.yscale('log')
 plt.show()
 
+# Graficamos las curvas de isocosto -------------------------------------------------
+# Generar una malla para las curvas de iso-costos
 
-# Le agregamos ruido a la matriz A y probamos las soluciones obtenidas con ruido N=1000 veces y calculamos el error promedio de cada solución
-N = 1000
-error_promedio = 0
-error_promedio_2 = 0
-error_promedio_svd = 0
-
-def add_noise(A, b, noise_std):
-    A_noise = A + np.random.normal(0, noise_std, A.shape)
-    b_noise = b + np.random.normal(0, noise_std, b.shape)
-    return A_noise, b_noise
-
-# for i in range(N):
-#     # Generamos A con ruido
-#     # A_ruido = A + np.random.normal(0, 0.1, (n,d))
-#     A_ruido, b_ruido = add_noise(A, b, 0.1)
-#     error_promedio += np.linalg.norm(A_ruido @ x - b_ruido)
-#     error_promedio_2 += np.linalg.norm(A_ruido @ x_2 - b_ruido)
-#     error_promedio_svd += np.linalg.norm(A_ruido @ x_svd - b_ruido)
-
-error_avg_F1 = 0
-error_avg_F2 = 0
-error_avg_SVD = 0
-
-for _ in range(N):
-    A_noisy, b_noisy = add_noise(A, b, 0.5)
-
-    error_avg_F1 += np.linalg.norm(A_noisy @ x - b_noisy)
-    error_avg_F2 += np.linalg.norm(A_noisy @ x_2 - b_noisy)
-    error_avg_SVD += np.linalg.norm(A_noisy @ x_svd - b_noisy)
-
-
-
-error_avg_F1 /= N
-error_avg_F2 /= N
-error_avg_SVD /= N
-
-print("Error promedio de A*x-b con ruido en F1: ", error_avg_F1)
-print("Error promedio de A*x-b con ruido en F2: ", error_avg_F2)
-print("Error promedio de A*x-b con ruido en SVD: ", error_avg_SVD)
+# Hcaemos PCA para reducir la dimensión a 2
+U, S, Vt = np.linalg.svd(A)
+A_2 = A @ Vt.T[:,:2]
